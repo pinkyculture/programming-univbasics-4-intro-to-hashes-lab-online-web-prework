9393d7bb4e06af4bb661486c62c8fbb953442379
interactive-cache-8375 <randomblue1@yahoo.com>
2019-10-09 23:41:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,13 +37,10 @@ end
 
 
 def update_counting_hash(hash, key)
-if hash[key] 
+if hash[key]
+  hash[key] += 1
 else
- key + 1
- 
-  
-
-end
+ end
 
 
   # given a hash an a key as parameters, return an updated hash
